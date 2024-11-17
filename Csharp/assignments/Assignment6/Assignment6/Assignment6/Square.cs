fd7c1cabf3c8e1c81969e54ce39a96d6ee8d0cf1
Durgasai Kottu <durgasaik@infinite.com>
2024-11-17 21:35:26 +0530

    Assigment6

---
+++ Csharp/assignments/Assignment6/Assignment6/Assignment6/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Square
+    {
+      static void Main(string[] args)
+      { Console.WriteLine("Enter numbers with spaces"); 
+       string input = Console.ReadLine(); 
+       input.Split().Select(int.Parse).Where(n => n * n > 20).ToList().ForEach(n => Console.WriteLine($"{n}-{n * n}")); 
+       Console.ReadLine();
+      }
+    }
+}
