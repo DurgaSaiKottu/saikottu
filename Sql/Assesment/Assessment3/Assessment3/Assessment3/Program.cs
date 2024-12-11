d1c83389ead80d4999e5fd67a5fb58747d050f71
Durgasai Kottu <durgasaik@infinite.com>
2024-12-11 12:38:00 +0530

    assessment3

--- Sql/Assesment/Assessment3/Assessment3/Assessment3/Program.cs
+++ Sql/Assesment/Assessment3/Assessment3/Assessment3/Program.cs
@@ -8,69 +8,69 @@ using System.Data.SqlClient;
 
 namespace Assessment3
 {
-        class Program
+    class Program
+    {
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        static void Main(string[] args)
         {
-            public static SqlConnection conn;
-            public static SqlCommand cmd;
-            static void Main(string[] args)
-            {
-                insert();
-                Console.Read();
-            }
+            insert();
+            Console.Read();
+        }
 
-            static SqlConnection Connection()
+        static SqlConnection Connection()
+        {
+            conn = new SqlConnection("Data Source=ICS-LT-D244D6FL;Initial Catalog=Assessment;" +
+                "Integrated Security=true;");
+            Console.WriteLine("Connected to a database:");
+            conn.Open();
+            return conn;
+        }
+        static void insert()
+        {
+            try
             {
-                conn = new SqlConnection("Data Source=ICS-LT-D244D6FL;Initial Catalog=Assessment;" +
-                    "Integrated Security=true;");
-                Console.WriteLine("Connected to a database:");
-                conn.Open();
-                return conn;
-            }
-            static void insert()
-            {
-                try
+                Connection();
+                Console.WriteLine("Connected to database...");
+
+                cmd = new SqlCommand("sp_insert", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                Console.Write("Enter Product name : ");
+                string Pname = Console.ReadLine();
+
+                Console.Write("Enter the Product price : ");
+                int P_price = Convert.ToInt32(Console.ReadLine());
+
+                int Discount_price = P_price;
+
+                cmd = new SqlCommand("insert into Products_Details values(@ProductName,@Price,@DiscountedPrice)", conn);
+                Console.WriteLine();
+                Console.WriteLine("Query Executed...");
+
+                cmd.Parameters.AddWithValue("@ProductName", Pname);
+                cmd.Parameters.AddWithValue("Price", P_price);
+                cmd.Parameters.AddWithValue("@DiscountedPrice", Discount_price);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
                 {
-                    Connection();
-                    Console.WriteLine("Connected to database...");
-
-                    cmd = new SqlCommand("sp_insert", conn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    Console.Write("Enter Product name : ");
-                    string Pname = Console.ReadLine();
-
-                    Console.Write("Enter the Product price : ");
-                    int P_price = Convert.ToInt32(Console.ReadLine());
-
-                    int Discount_price = P_price;
-
-                    cmd = new SqlCommand("insert into ProductsDetails values(@ProductName,@Price,@DiscountedPrice)", conn);
                     Console.WriteLine();
-                    Console.WriteLine("Query Executed...");
-
-                    cmd.Parameters.AddWithValue("@ProductName", Pname);
-                    cmd.Parameters.AddWithValue("Price", P_price);
-                    cmd.Parameters.AddWithValue("@DiscountedPrice", Discount_price);
-
-                    int result = cmd.ExecuteNonQuery();
-
-                    if (result > 0)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("*** Product Details inserted Successfully ***");
-                    }
-                    else Console.WriteLine("Product details not inserted");
+                    Console.WriteLine("*** Product Details inserted Successfully ***");
                 }
-                catch (Exception product)
-                {
-                    Console.WriteLine($"Error {product.Message}");
-                }
-                finally
-                {
-                    conn.Close();
-                }
-
+                else Console.WriteLine("Product details not inserted");
             }
+            catch (Exception product)
+            {
+                Console.WriteLine($"Error {product.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
         }
     }
+}
 
